f14580d4e0f0adcda6813c3f0c2dbc1104e7f70a
Daniel Schneider <daschne8@gmail.com>
2019-01-28 00:59:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function takeANumber(line, name){
   line.push(name);
   var linePosition = line.length ;
-  return "Welcome, ${name}. You are number ${linePosition} in line.";
+  var greeting = "Welcome, ${name}. You are number ${linePosition} in line.";
+  return greeting;
 }
 function nowServing(){
 return 5;
